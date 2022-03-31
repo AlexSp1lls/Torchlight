3188523f89a79355d3356389fe2965d44959dbe7
AlexSp1lls <victorisglad@gmail.com>
2022-03-31 15:23:59 -0400

    Closed

--- TD/Assets/Scripts/GameManager.cs
+++ TD/Assets/Scripts/GameManager.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] GameObject spawnPoint;
+    [SerializeField] GameObject[] enemies;
+    [SerializeField] int maxEnemies;
+    [SerializeField] int totalEnemies;
+    [SerializeField] int enemiesToSpawn;
+
+    private int enemiesOnScreen = 0;
+
     // Start is called before the first frame update
     void Start()
     {
